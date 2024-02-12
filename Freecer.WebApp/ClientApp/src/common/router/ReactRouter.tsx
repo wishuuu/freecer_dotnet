afc5378cf1b9e2b7b485b1e56026855f2496ef9f
Oskar Wiszowaty <ppwiszu@gmail.com>
2024-02-12 17:43:34 +0100

    Tailwind and react router

---
+++ Freecer.WebApp/ClientApp/src/common/router/ReactRouter.tsx
@@ -0,0 +1,20 @@
+﻿import {ComponentType} from "react";
+
+
+interface RouteObject {
+    path: string;
+    component: ComponentType;
+    loggedIn: boolean;
+    authorization?: (roles: string[]) => boolean;
+    moduleAccess?: string;
+}
+
+export default function CreateRoutes() : RouteObject[] {
+    return [
+        {
+            path: "/",
+            component: () => <div>Home</div>,
+            loggedIn: false,
+        },
+    ];
+}
