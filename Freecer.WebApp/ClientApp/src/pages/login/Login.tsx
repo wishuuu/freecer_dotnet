8d8faad15c292f195e2e460592a1a54cb9686f36
Oskar Wiszowaty <ppwiszu@gmail.com>
2024-02-21 00:12:19 +0100

    Login Card

--- Freecer.WebApp/ClientApp/src/pages/login/Login.tsx
+++ Freecer.WebApp/ClientApp/src/pages/login/Login.tsx
@@ -1,30 +1,42 @@
 ﻿import {Input} from "@/components/ui/input.tsx";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {useAuth} from "@/common/hooks/useAuth.ts";
 import {Button} from "@/components/ui/button.tsx";
-import {useUser} from "@/common/hooks/useUser.ts";
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card.tsx";
+import {Label} from "@/components/ui/label.tsx";
 
 
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const { login, logout } = useAuth();
-    const { user } = useUser();
-    
-    useEffect(() => {
-        
-    });
-    
+    const { login } = useAuth();
+
     return (
-        <div>
-            <h1>Login</h1>
-            <Input placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-            <Input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <Button onClick={() => login(username, password)}>Login</Button>
-            <Button onClick={() => logout()}>Logout</Button>
-            <h2>User</h2>
-            <pre>{JSON.stringify(user, null, 2)}</pre>
-        </div>
+        <Card className={"w-[350px] m-auto"}>
+            <CardHeader>
+                <CardTitle>
+                    Login
+                </CardTitle>
+                <CardDescription>
+                    Wprowadź swoje dane logowania
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className={"grid w-full items-center gap-4"}>
+                    <div className={"flex flex-col space-y-1.5"}>
+                        <Label>Nazwa użytkownika</Label>
+                        <Input placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                    </div>
+                    <div className={"flex flex-col space-y-1.5"}>
+                        <Label>Hasło</Label>
+                        <Input placeholder="Password" type={"password"} value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
+                </div>
+            </CardContent>
+            <CardFooter className={"flex justify-between"}>
+                <Button onClick={() => login(username, password)}>Login</Button>
+            </CardFooter>
+        </Card>
     )
 }
 
